68c2836170f0bb3ed82979d66475fafce2566bf0
jwlee <jwlee1130@naver.com>
2024-08-22 17:19:08 +0900

    zz

--- src/kosta/exam/test1.java
+++ src/kosta/exam/test1.java
@@ -2,11 +2,8 @@ package kosta.exam;
 
 public class test1 {
 	void test() {
-<<<<<<< HEAD
 		System.out.println("zz");
 		System.out.println("아아");
-=======
 		System.out.println("aa");
->>>>>>> cart
 	}
 }
